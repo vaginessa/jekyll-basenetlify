8e075a0227a7966dd825008fef3779d0ba93e34b
vaginessa <vaginessa@users.noreply.github.com>
2018-09-01 16:32:18 +0200

    Create 2018-01-09-awesome-pwa.m

---
+++ _posts/2018-01-09-awesome-pwa.m
@@ -0,0 +1,47 @@
+﻿# awesome-pwa [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome) [![Build Status](https://travis-ci.org/hemanth/awesome-pwa.svg?branch=master)](https://travis-ci.org/hemanth/awesome-pwa)
+> Useful resources for creating Progressive Web Apps
+
+*Inspired by the [awesome](https://github.com/sindresorhus/awesome) list thing.*
+
+## What is a Progressive Web App ?
+
+> A Progressive Web App uses modern web capabilities to deliver an app-like user experience. They evolve from pages in browser tabs to immersive, top-level apps, leveraging the web's low friction.
+
+_Source:_ [Google Developers - Progressive Web Apps](https://developers.google.com/web/progressive-web-apps/)
+
+## Table of contents
+
+* [Apps](#apps)
+* [Tutorials](#tutorials)
+* [Articles](#articles)
+* [Videos](#videos)
+* [Tools](#tools)
+* [Kits](#kits)
+* [Courses](#courses)
+
+## Apps
+
+* [A selection of Progressive Web Apps](https://pwa.rocks/)
+* [A fresher selection of Progressive Web Apps](https://outweb.io/)
+* [AirHorner](https://airhorner.com/)
+* [AlarmDJ](https://alarmdj.com)
+* [Aliexpress](https://m.aliexpress.com/?tracelog=wwwhome2mobilesitehome)
+* [Chrome Developer Summit](https://developers.google.com/)
+* [Colosseum](https://naramsim.github.io/Colosseum/)
+* [Closerintime](https://closerinti.me)
+* [Currency Converter](https://www.currency-x.com)
+* [Datememe](https://www.datememe.com)
+* [Demo PWA](https://github.com/gokulkrishh/demo-progressive-web-app)
+* [Dev.Opera](https://dev.opera.com/)
+* [DevSpace](https://app.devspace.io/)
+* [Expense Manager](https://demo.vaadin.com/expense-manager/)
+* [Exterium](https://www.extrange-labs.com/exterium/)
+* [FF-Platfrom-Status](https://platform-status.mozilla.org)
+* [Firefox Platform Status](https://platform-status.mozilla.org/)
+* [Flipkart Lite](http://www.flipkart.com) ([eng blog post](https://medium.com/progressive-web-apps/building-flipkart-lite-a-progressive-web-app-2c211e641883), [case study](https://developers.google.com/web/showcase/2016/flipkart))
+* [GitHub Explorer](https://github-e.com/)
+* [Google I/O](https://events.google.com/io2016/)
+* [GPA Calculator](https://gpacalculator.io)
+* [Housing Go](http://www.housing.com)
+* [Meat Scope](https://meatscope.camera/)
+* [Medium](https://medium.com/)
